f77a293bf658b9b249e261b58caa88d056648fa1
JiangTaoHou <925147954@qq.com>
2023-07-08 21:21:27 +0800

    1.从对象中获取值

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -11,9 +11,9 @@ public class MainTest {
     public void testMultiplication(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10,product.getAmount());
+        assertEquals(new Dollar(10),product.getAmount());
         product = five.times(3);
-        assertEquals(15,product.getAmount());
+        assertEquals(new Dollar(15),product.getAmount());
     }
     @Test
     public void testEquality(){
