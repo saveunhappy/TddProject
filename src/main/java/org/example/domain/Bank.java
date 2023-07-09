4a146190e734eb29d30526c6fc9ca6abe5e185d3
JiangTaoHou <925147954@qq.com>
2023-07-09 22:20:26 +0800

    1.类型判断

--- src/main/java/org/example/domain/Bank.java
+++ src/main/java/org/example/domain/Bank.java
@@ -4,6 +4,7 @@ import org.example.service.Expression;
 
 public class Bank {
     public Money reduce(Expression source,String to){
+        if(source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
